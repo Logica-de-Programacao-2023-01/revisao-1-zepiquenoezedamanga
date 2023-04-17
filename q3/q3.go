8c81f8b71251d240dca92c29c4fcf493861e860b
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-04-17 19:55:26 -0200

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,25 @@
 package q3
 
+import (
+	"fmt"
+	"sort"
+)
+
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+	if len(numbers) == 0 {
+		return 0, 0, 0, fmt.Errorf("lista vazia")
+	}
+	var maior int
+	var menor int
+	sort.Ints(numbers)
+	menor = (numbers[0])
+	maior = (numbers[len(numbers)-1])
+	var media float64
+	var soma int
+	for _, valor := range numbers {
+		soma += valor
+		media = float64(soma) / float64(len(numbers))
+
+	}
+	return menor, maior, media, nil
 }
