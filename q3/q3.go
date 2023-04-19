9f199bd85b405f4f45dc4520b0f48b98615ea7cd
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-04-18 22:10:28 -0200

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -3,23 +3,25 @@ package q3
 import "fmt"
 
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	expectedMax := -9999999999
-	expectedMin := 99999999999
-	var soma, embaixo, expectedAverage float64
+
+	maior := -9999999999
+	menor := 99999999999
+	var soma float64
+	var contagem float64
+	var media float64
 	if len(numbers) == 0 {
 		return 0, 0, 0, fmt.Errorf("Lista vazia")
 	}
 	for i := 0; i < len(numbers); i++ {
 		soma += float64(numbers[i])
-		embaixo++
-		if numbers[i] > expectedMax {
-			expectedMax = numbers[i]
+		contagem++
+		if numbers[i] > maior {
+			maior = numbers[i]
 		}
-		if numbers[i] < expectedMin {
-			expectedMin = numbers[i]
+		if numbers[i] < menor {
+			menor = numbers[i]
 		}
 	}
-	expectedAverage = (soma - float64(expectedMin) - float64(expectedMax)) / (embaixo - 2)
-	return expectedMax, expectedMin, expectedAverage, nil
+	media = (soma - float64(menor) - float64(maior)) / (contagem - 2)
+	return maior, menor, media, nil
 }
