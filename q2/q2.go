5f9e965e88c0956ace9066d3cbeb0938f44f337e
João Gabriel <126472751+joaodelabio@users.noreply.github.com>
2023-04-19 16:23:23 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,27 @@
-package q2
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	AverageLettersPerWord("O rato roeu a roupa do rei de Roma")
+
+}
 
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
+
+	palavras := strings.Split(text, " ")
+	soma := 0.
+	for x := 0; x < len(palavras); x++ {
+		soma += float64(len(palavras[x]))
+	}
+
+	media := soma / float64(len(palavras))
+
+	fmt.Printf("%f", media)
+
 	return 0, nil
 }
