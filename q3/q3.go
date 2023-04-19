3cd1b57915b0e51d18ed17d6b59270c8e332a910
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-04-18 22:05:25 -0200

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -13,10 +13,11 @@ func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
 	}
 	maior := -9999999999
 	menor := 99999999999
-	var soma int
-	var contagem int
+	var soma float64
+	var contagem float64
+	var media float64
 	for i := 1; i < len(numbers)-1; i++ {
-		soma += numbers[i]
+		soma += float64(numbers[i])
 		contagem++
 		if numbers[i] > maior {
 			maior = numbers[i]
@@ -25,7 +26,7 @@ func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
 			menor = numbers[i]
 		}
 	}
-	media := float64(soma) / float64(len(numbers)-2)
+	media = (soma - float64(menor) - float64(maior)) / (contagem - 2)
 
 	return maior, menor, media, nil
 }
