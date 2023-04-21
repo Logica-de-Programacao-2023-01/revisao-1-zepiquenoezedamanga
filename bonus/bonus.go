cd5cc1729ed155425328ca63a83b5638fccb5c1e
João Gabriel <126472751+joaodelabio@users.noreply.github.com>
2023-04-21 11:40:44 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,6 +1,24 @@
 package bonus
 
+import "fmt"
+
 func CalculateDamage(characterLevel, enemyLevel int) (int, error) {
-	// Implemente sua solução aqui
+	if characterLevel == enemyLevel {
+		fmt.Println(characterLevel * 7)
+	} else if characterLevel < enemyLevel {
+		fmt.Println(characterLevel * 5)
+	} else if enemyLevel > 100 {
+		fmt.Println(characterLevel * 2)
+	} else if characterLevel > enemyLevel {
+		fmt.Printf(characterLevel * 10)
+	} else if characterLevel > 100 {
+		fmt.Println(characterLevel * 20)
+	} else if characterLevel-enemyLevel > 20 {
+		fmt.Printf(characterLevel * 5)
+	} else if characterLevel-enemyLevel < 20 {
+		fmt.Println(characterLevel * 2)
+	} else if characterLevel < 0 || enemyLevel < 0 {
+		return 0, fmt.Errorf("nível invalido")
+	}
 	return 0, nil
 }
